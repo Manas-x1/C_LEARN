0aaac1168f19c59c3ecfd10840cd0140e1806c02
Manas Upadhayay <manas9399upadhyay@gmail.com>
2024-10-05 12:57:57 +0000

    ig thats enough for the whole calculator thing, mobing on to the school database or whatever

--- Projects/TestProject1_calculater.cpp
+++ Projects/TestProject1_calculater.cpp
@@ -2,13 +2,17 @@
 #include <cmath>
 using namespace std;
 
-
-
 void welcome()
 {
     string GREEN = "\033[32m"; // Green color
+    string RED = "\033[31m"; // Red color
     string RESET = "\033[0m"; // Reset to default color
     cout << GREEN << "welcome to the calculator"<< RESET << endl;
+    cout << GREEN << "you can use the following operators: " << RESET << endl;
+    cout << RED << "+, -, *, /, %, ^ (for two values)" << RESET << endl;
+    cout << RED << "Sq, Cube, sroot, croot, !, log, trig (for single value)" << RESET << endl;
+    cout << RED << "exit(to you know what;D)" << RESET << endl;
+
     
 }
 //funtions for calculator
@@ -33,15 +37,25 @@ double mul(double a, double b)
 }
 //division
 double divi(double a, double b)
-{
+{   
+    if(b == 0){
+        cout << "not defined" << endl;
+        return 0;
+    }else{
     double div = a / b;
     return div;
+    }
 }
 //modulus
 int mod(int a, int b)
 {
+    if(b == 0){
+        cout << "not defined" << endl;
+        return 0;
+    }else{
     double mod = a % b;
     return mod;
+    }
 }
 //exponantial
 double power(double a, double b)
@@ -97,8 +111,11 @@ double trig(string t ,double a)
 {
     //defining the variables
     double si;
+    double arcsi;
     double co;
+    double arcco;
     double ta;
+    double arcta;
     double se;
     double cose;
     double ct;
@@ -108,15 +125,30 @@ double trig(string t ,double a)
     double si = sin(a);
     return si;
     }
+    //arcsin
+    else if (t == "arcsin"){
+    double arcsi = asin(a);
+    return arcsi;
+    }
     //cos
     else if (t == "cos"){
     double co = cos(a);
     return co;
     }
+    //arccos
+    else if (t == "arccos"){
+    double arcco = acos(a);
+    return arcco;
+    }
     //tan
     else if (t == "tan"){
     double ta = tan(a);
     return ta;
+    }
+    //arctangent
+    else if (t == "arctangent"){
+    double arcta = atan(a);
+    return arcta;
     }
     //sec
     else if (t == "sec"){
@@ -138,9 +170,15 @@ double trig(string t ,double a)
     cout << "invalid operator" << endl;
     }
 }
-
-
-
+void help(){
+    string GREEN = "\033[32m"; // Green color
+    string RED = "\033[31m"; // Red color
+    string RESET = "\033[0m"; // Reset to default color
+    cout << GREEN << "you can use the following operators: " << endl;
+    cout << RED << "+, -, *, /, %, ^ (for two values)" << endl;
+    cout << RED << "Sq, Cube, sroot, croot, !, log, trig (for single value)" << RESET << endl;
+    
+}
 
 
 int main()
@@ -154,11 +192,10 @@ int main()
 
     while (op != "exit"){
     //taking the input from the user
-    cout << "Enter the value of a: ";
-    cin >> num1;
     cout << "Enter the operator: ";
     cin >> op;
-    
+    cout << "Enter the value of a: ";
+    cin >> num1;
 
     //checking if the calcuulation is for single value or dual value
     //for dual value
@@ -173,7 +210,7 @@ int main()
     {
     cout << "The sub is: " << sub(num1, num2) << endl; //calling sub function
     }
-    else if (op == "*")
+    else if (op == "x")
     {
     cout << "The mul is: " << mul(num1, num2) << endl; //calling mul function
     }
@@ -194,29 +231,32 @@ int main()
     cout << "Invalid operator" << endl; //error handeling
     continue;
     }
-
+    }
+    else if (op == "help"){
+    help();
+    continue;
     }
     //for single value
     else{
-    if (op == "S"){
+    if (op == "Sq"){
     cout << "The square is: " << square(num1) << endl; //calling square function
     }
-    else if (op == "C"){
+    else if (op == "Cube"){
     cout << "The cube is: " << cube(num1) << endl; //calling cube function
     }
-    else if (op == "s"){
+    else if (op == "sroot"){
     cout << "The squareroot is: " << squareroot(num1) << endl; //calling squareroot function
     }
-    else if (op == "c"){
+    else if (op == "croot"){
     cout << "The cuberoot is: " << cuberoot(num1) << endl; //calling cuberoot function
     }
     else if (op == "!"){
     cout << "The factorial is: " << factorial(num1) << endl; //calling factorial function
     }
-    else if (op == "l"){
+    else if (op == "log"){
     cout << "The log is: " << log(num1) << endl; //calling log function
     }
-    else if (op == "t"){ //trigonometric functions
+    else if (op == "trig"){ //trigonometric functions
         string t;
         cout << "Enter the trigonometric function: ";
         cin >> t;
